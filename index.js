0502335c04e3684ae018877cb5f26218f421be2b
grayraw <grayraw@gmail.com>
2018-05-19 00:39:33 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const safeJsonStringify = require('safe-json-stringify');
+require('events').EventEmitter.defaultMaxListeners = 99;
+
+const startPage = 'https://www.cannondale.com/en/International';
+// const startPage = 'https://www.cannondale.com/en/International/Products/ProductCategory.aspx?nid=3cfe9447-598e-41c6-a8b4-5c0fe9196a80';
+const filters = ['[data-tab=Mountaintab] + .ddBox .col-xs-2 ul li a', '.viewProducts'];
+// const filters = ['.viewProducts'];
+
+let browser;
+let page;
+
+async function grabAPage(startPage, filters){
+    // const browser = await puppeteer.launch({headless: false});
+    // await page.goto(startPage, {timeout: 0});
+    // let pagesContent = [await page.content()];
+  //   await page.screenshot({path: 'example.png'});
+
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    page.setJavaScriptEnabled(true);
+    let pagesContent = await getContent([startPage]);
+
+    let i = 0;
+    while (i < filters.length){
+        // debugger;
+        let $pagesContent = pagesContent.map((page)=>{
+            return cheerio.load(page);
+        });
+        let links = [];
+        let filter = filters[i];
+        $pagesContent.map( ($page) =>{
+            let pageLinks = [];
+            $page(filter).map(function (i, elem) {
+                pageLinks.push(this.attribs.href);
+            });
+            links = [...links, ...pageLinks];
+        });        
+
+        pagesContent = await getContent(links);
+
+        i++;
+    }
+
+    debugger;    
+    console.log('waiting');
+  
+    // await browser.close();
+}
+
+async function getContent(links){
+    let formattedLinks = filterLinks(links, startPage);
+    // debugger;
+    formattedLinks = [formattedLinks[0]];
+    let newContent = [];
+    await Promise.all(formattedLinks.map(async (link)=>{
+        const p = await browser.newPage();
+        p.setJavaScriptEnabled(true);
+        await page.setRequestInterception(true);
+        page.on('request', interceptedRequest => {
+          if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
+            interceptedRequest.abort();
+          else
+            interceptedRequest.continue();
+        });
+        await p.goto(link, {
+            waitUntil: 'networkidle2',                    
+            timeout: 0});
+        
+        newContent.push(await p.content());
+        // p.close();
+    }));
+    return newContent;
+}
+
+
+function filterLinks(links, startPage){
+    let filteredLinks = links.map((link)=>{
+        if (/^http/g.test(link)) {
+            return link;
+        }
+        else if (/^\//g.test(link)) {
+            let rootLink = startPage.match(/^[\w]*:\/\/.+?(?=\/)/g);
+            return rootLink + link;
+        } else {
+            return startPage + link;
+        }
+    });
+    return filteredLinks;
+}
+
+console.log('started');
+grabAPage(startPage, filters);
+
+// console.log($('[data-tab=Mountaintab] + .ddBox .col-xs-2 ul').text());
